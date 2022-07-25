d2bc8bc7e699cf4525fa156a353005ab060cfdaf
Madziula99 <100pkamagda@gmail.com>
2022-07-25 23:16:16 +0200

    Add sort by column option

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import data from './data/data.json';
 import FlightsTable from './components/FlightTable';
 
 /*
-1. load all data
-2. divide data by arr/dep and thow away the unnecessary fields
-3. extract from date converted to local time
-4. design the Table component (prop is table of object)
-5. implement Row component to represent single flight (take object of flight as prop)
-6. in Table map through table and append Row for every flight
+1. load all data +
+2. divide data by arr/dep and thow away the unnecessary fields +
+3. extract from date converted to local time +
+4. design the FlightTable component (pass array of objects) +
+5. implement Row component to represent single flight (take object of flight as prop) +/-
+6. in Table map through table and append Row for every flight +
 7. sort func (take field value to sort by) return new array of sorted obj
-8. srting table of objects: handle click for table header specifying the sort option
+8. sort table of objects: handle click for table header specifying the sort option
 9. investigate media query
 */
 
 function App() {
 
-  const [airportData, setAirportData] = useState([]);
   const [arrivals, setArrivals] = useState([]);
   const [departures, setDepartures] = useState([]);
 
@@ -32,7 +31,6 @@ function App() {
         else setArrivals(current => [...current, flight]);
       })
     }
-    setAirportData(data);
   }, []);
 
   function changeDirection() {
@@ -47,7 +45,7 @@ function App() {
             <button onClick={changeDirection}>{direction === 'Arrivals' ? 'Departures' : 'Arrivals'}</button>
           </div>
           <div className='flightsTable'>
-            <FlightsTable flights={arrivals} direction={direction} />
+            <FlightsTable flights={direction === 'Arrivals' ? arrivals : departures} direction={direction} />
           </div>
         </div>
     </React.Fragment>
--- src/components/FlightTable.js
+++ src/components/FlightTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './flightTable.css';
 
 function readableDate(isoDate) {
@@ -12,28 +12,44 @@ function readableDate(isoDate) {
     return foo
 }
 
+
 const FlightsTable = ({flights, direction}) => {
+    const [listChange, setListChange] = useState([])
+
+    function dynamicSort(property) {
+        return function (a,b) {
+            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result;
+        }
+    }
+
+    function sortFlights(flights, property) {
+        flights.sort(dynamicSort(property));
+        setListChange([...flights]);
+    }
+
+
     return (
         <table>
             <thead>
                 <tr>
-                    <th>Airport Name</th>
-                    <th>Flight Number</th>
-                    {(direction==='Departures') ? <th>Gate</th> : null}
-                    <th>Scheduled Time</th>
-                    <th>Status</th>
-                    <th>Terminal</th>
+                    <th onClick={() => sortFlights(flights, 'fnr')}>Flight Number</th>
+                    <th onClick={() => sortFlights(flights, 'apname')}>Airport Name</th>
+                    <th onClick={() => sortFlights(flights, 'sched')}>Time</th>
+                    {(direction==='Departures') ? <th onClick={() => sortFlights(flights, 'gate')}>Gate</th> : null}
+                    <th onClick={() => sortFlights(flights, 'status')}>Status</th>
+                    <th onClick={() => sortFlights(flights, 'terminal')}>Terminal</th>
                 </tr>
             </thead>
             <tbody>
                 {flights.map((flightInfo, i) => {
                     return (
                     <tr key={i}>
-                        <td>{flightInfo.apname}</td>
                         <td>{flightInfo.fnr}</td>
-                        {(direction==='Departures') ? <td>{flightInfo.gate}</td> : null}
+                        <td>{flightInfo.apname}</td>
                         <td>{readableDate(flightInfo.sched)}</td>
-                        <td>{flightInfo.status}</td>
+                        {(direction==='Departures') ? <td>{flightInfo.gate}</td> : null}
+                        <td>{flightInfo.status.toLowerCase()}</td>
                         <td>{flightInfo.terminal}</td>
                     </tr>
                     )
